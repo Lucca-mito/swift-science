44d6aad36422c6e7fb332185c9d81932818e6ca4
Lucca de Mello <luccasukman@gmail.com>
2023-04-26 03:24:14 -0700

    Improve `DiscreteDistribution` documentation

--- Sources/Science/Probability/DiscreteDistribution.swift
+++ Sources/Science/Probability/DiscreteDistribution.swift
@@ -1,9 +1,19 @@
 //
 //  DiscreteDistribution.swift
-//  
+//
 //
 //  Created by Lucca de Mello on 4/15/23.
 //
 
-/// 
+import RealModule
+
+/// A [discrete probability distribution][wiki].
+///
+/// [wiki]:https://en.wikipedia.org/wiki/Probability_distribution#Discrete_probability_distribution
 public protocol DiscreteDistribution: ProbabilityDistribution {}
+
+// Note that DiscreteDistribution does not inherit from DistributionWithMean. Not every discrete
+// distribution has a mean! For example,
+// - The distribution with CDF `F(n) = 1 - 1/n` over the integers `n = 1, 2, ...` never has a mean.
+// - The Zeta distribution may or may not have a mean.
+// See also the St. Petersburg paradox.
