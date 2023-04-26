4da5e70b8f59ece12ff8ee97c0380d6c62bc3bca
Lucca de Mello <luccasukman@gmail.com>
2023-04-26 03:31:35 -0700

    Add `BoundedDistribution` documentation

--- Sources/Science/Probability/BoundedDistribution.swift
+++ Sources/Science/Probability/BoundedDistribution.swift
@@ -5,7 +5,11 @@
 //  Created by Lucca de Mello on 4/22/23.
 //
 
+/// A probability distribution with a lower bound and an upper bound.
 public protocol BoundedDistribution: ProbabilityDistribution {
+    /// The lowest ``ProbabilityDistribution/Value`` with a positive probability.
     var min: Value { get }
+    
+    /// The highest ``ProbabilityDistribution/Value`` with a positive probability.
     var max: Value { get }
 }
