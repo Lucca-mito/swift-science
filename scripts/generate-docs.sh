c6e5a739d27eeed0e6805cc4894b602a26248783
Lucca de Mello <luccasukman@gmail.com>
2023-05-06 16:02:05 -0700

    Add `preview-docs` script

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -5,4 +5,7 @@ swift package \
     --disable-indexing \
     --transform-for-static-hosting \
     --hosting-base-path swift-science \
-    --output-path docs
+    --output-path docs \
+    --source-service github \
+    --source-service-base-url https://github.com/lucca-mito/swift-science/blob/main \
+    --checkout-path /Users/lucca/Developer/swift-science
