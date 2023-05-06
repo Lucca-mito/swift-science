cd7211fc9d645faab3723405eb9cff34d5281d62
Lucca de Mello <luccasukman@gmail.com>
2023-05-06 13:15:36 -0700

    Improve formatting of `scripts/generate-docs.sh`

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,6 +1,8 @@
-swift package --allow-writing-to-directory docs \
-    generate-documentation --target Science \
+swift package \
+    --allow-writing-to-directory docs \
+    generate-documentation \
+    --target Science \
     --disable-indexing \
     --transform-for-static-hosting \
-    --hosting-base-path "swift-science" \
+    --hosting-base-path swift-science \
     --output-path docs
