35e2d3e4ec3cdad245354423f3aab1845c815627
Lucca de Mello <luccasukman@gmail.com>
2023-05-05 21:14:31 -0700

    Generate docs

---
+++ scripts/generate-docs.sh
@@ -0,0 +1,6 @@
+swift package --allow-writing-to-directory docs \
+    generate-documentation --target Science \
+    --disable-indexing \
+    --transform-for-static-hosting \
+    --hosting-base-path "swift-science" \
+    --output-path docs
