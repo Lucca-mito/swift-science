1af5af16af83d07f0caa1ae2556d9205082e3e05
Lucca de Mello <luccasukman@gmail.com>
2023-05-10 01:18:53 -0700

    Improve formatting of `momentGeneratingFunction` documentation

--- Sources/Science/Probability/Moments.swift
+++ Sources/Science/Probability/Moments.swift
@@ -20,9 +20,22 @@ public protocol Moments: DistributionWithVariance {
     ///
     /// [moment-generating function]:https://en.wikipedia.org/wiki/Moment-generating_function
     ///
-    /// - Returns: The expected value of `.pow(.e, t * x)` where `x` follows this distribution.
+    /// - Returns: The expected value of exp(`t` 𝑋) where 𝑋 is a random variable following this distribution.
     ///
+    /// ## Usage notes
     /// - Precondition: `t` ≥ 0
-    /// - Note: `momentGeneratingFunction(0)` is always 1.
+    ///
+    /// `momentGeneratingFunction(0)` is always 1.
+    ///
+    /// ## Example
+    /// ```swift
+    /// let normal = NormalDistribution<Double>.standard
+    /// print( normal.momentGeneratingFunction(1) ) // Approximately 1.64872
+    /// print( normal.sample(count: 1_000_000).map(Double.exp).mean() ) // Approximately 1.64872
+    /// ```
+    ///
+    /// ## Discussion
+    /// The moment-generating function is commonly abbreviated as MGF.
+    /// Every probability distribution that has an MGF has a _unique_ MGF.
     func momentGeneratingFunction(_ t: some BinaryInteger) -> Statistic
 }
