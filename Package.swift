e33bdac51b90cbb6dca9c0f11686c210621f0dc4
Lucca de Mello <luccasukman@gmail.com>
2023-05-16 15:59:24 -0700

    Remove excessive line breaks in `Package.swift`

--- Package.swift
+++ Package.swift
@@ -1,30 +1,18 @@
 // swift-tools-version: 5.8
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-science",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Science",
-            targets: ["Science"]),
+        .library(name: "Science", targets: ["Science"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Science",
-            dependencies: [
-                .product(name: "Numerics", package: "swift-numerics")
-            ]),
-        .testTarget(
-            name: "ScienceTests",
-            dependencies: ["Science"]),
+        .target(name: "Science", dependencies: [.product(name: "Numerics", package: "swift-numerics")]),
+        .testTarget(name: "ScienceTests", dependencies: ["Science"])
     ]
 )
