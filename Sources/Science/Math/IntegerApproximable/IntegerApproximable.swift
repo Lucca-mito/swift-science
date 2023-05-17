c29d8b04221955e4a20fced76f03af76657625d6
Lucca de Mello <luccasukman@gmail.com>
2023-05-17 02:27:04 -0700

    Remove unused import

--- Sources/Science/Math/IntegerApproximable/IntegerApproximable.swift
+++ Sources/Science/Math/IntegerApproximable/IntegerApproximable.swift
@@ -5,8 +5,6 @@
 //  Created by Lucca de Mello on 5/15/23.
 //
 
-import ComplexModule
-
 /// A numeric type that can be approximated from an integer.
 public protocol IntegerApproximable: Numeric {
     /// Creates a new value, rounded to the closest possible representation.
