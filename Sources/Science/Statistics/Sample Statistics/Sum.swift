50663bde540205f9b8544124d87f838b86c85eab
Lucca de Mello <luccasukman@gmail.com>
2024-08-13 21:36:41 -0400

    Fix (another) typo

--- Sources/Science/Statistics/Sample Statistics/Sum.swift
+++ Sources/Science/Statistics/Sample Statistics/Sum.swift
@@ -8,14 +8,8 @@
 import Numerics
 
 extension Collection where Element: AdditiveArithmetic {
-    // TODO: Consider, either by default or as an option, any of the following:
-    // - If Element is AlgebraicField: use RelaxedArithmetic.add from Swift Numerics.
-    // - If Element is FloatingPoint: sort before summing for greater precision.
-    // - If Element is BinaryInteger: cast to a higher-capacity type, sum, then cast back.
-    //   This avoids integer overflows when the total sum does not overflow but the partial sums do overflow.
-    // - If Element is BinaryFloatingPoint: cast to a higher-precision type, sum, then cast back.
     /// The sum of all the elements in the collection.
-    /// - Returns: The sum. Unlike other sample statistics, this always has the same as the `Element`s.
+    /// - Returns: The sum.
     ///
     /// ## Example
     /// ```swift
@@ -25,4 +19,11 @@ extension Collection where Element: AdditiveArithmetic {
     public func sum() -> Element {
         reduce(.zero, +)
     }
+    
+    // TODO: Consider adding any of the following as options:
+    // - If Element is AlgebraicField: use RelaxedArithmetic.add from Swift Numerics.
+    // - If Element is FloatingPoint: sort before summing for greater precision.
+    // - If Element is BinaryInteger: cast to a higher-capacity type, sum, then cast back.
+    //   This avoids integer overflows when the total sum does not overflow but the partial sums do overflow.
+    // - If Element is BinaryFloatingPoint: cast to a higher-precision type, sum, then cast back.
 }
